b8148f95072268e453f00210c06790dc02abbff8
stnDinus <111202214433@mhs.dinus.ac.id>
2024-06-26 11:12:52 +0700

    init

---
+++ src/main/java/id/ac/dinus/a112214433/App.java
@@ -0,0 +1,7 @@
+package id.ac.dinus.a112214433;
+
+public class App {
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!" );
+  }
+}
---
+++ src/test/java/id/ac/dinus/a112214433/AppTest.java
@@ -0,0 +1,12 @@
+package id.ac.dinus.a112214433;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
